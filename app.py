f7ac465d4a5d9c01e261664126024ed05afc8fd8
arzubek <arzubekm@gmail.com>
2021-03-02 22:50:54 +0900

    Added templates

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import recommendation
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/movie', methods=['GET'])
+@app.route('/')
+def homepage():
+    return render_template('form.html')
+
+@app.route('/movie', methods=['GET', 'POST'])
 def recommend_movies():
-    res = recommendation.results(request.args.get('title'))
-    return jsonify(res)
+    if request.method == 'POST':
+        # res = recommendation.results(request.args.get('title'))
+        # res = recommendation.results(request.form.value)
+        # print(res)
+        res = recommendation.results(request.form.get('title'))
+        return render_template('/index.html', list=res)
+    # res = recommendation.results(request.args.get('title'))
+    # return render_template('/index.html', list=res)
 
 if __name__ == '__main__':
     app.run(port=5000, debug = True)
