642f293ec7b0319b286a04a4051e06d4288f973b
arzubekm <arzubekm@gmail.com>
2021-05-11 17:09:13 +0900

    Working on autocomplete at form.html

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+import pandas as pd
 import recommendation
 
 app = Flask(__name__)
 CORS(app)
 
+def get_data():
+    data = pd.read_csv('dataset/movie_data.csv')
+    return data
+
 @app.route('/')
 def homepage():
-    return render_template('form.html')
+    data = get_data()
+    original_title = data.original_title
+    return render_template('form.html', movie_list=original_title)
 
 @app.route('/movie', methods=['GET', 'POST'])
 def recommend_movies():
